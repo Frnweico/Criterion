fa5ec5344e0746be740ae4846c014b4a8652e3d9
Leshi Taiwo <98643006+demi05@users.noreply.github.com>
2025-08-16 23:19:02 +0100

    fixed line issue on property page

--- src/Containers/HomeServices/HomeServices.tsx
+++ src/Containers/HomeServices/HomeServices.tsx
@@ -1,18 +1,47 @@
 import classes from './HomeServices.module.css';
 import Button from '../../Components/Button/Button';
-import homeServiceImg from '../../Assets/Images/homeServiceImg.png';
+import serviceImg1 from "../../Assets/Images/Services 1 - Image.png"
+import serviceImg2 from "../../Assets/Images/Services 2 - Image.png"
+import serviceImg3 from "../../Assets/Images/Services 3 - Image.png"
+import serviceImg4 from "../../Assets/Images/Services 4 - Image.png"
 import { useContext } from 'react';
 import { AppContext } from '../../Context/AppContext';
 
 const HomeServices = () => {
     const { scrollToRef } = useContext(AppContext);
+    const homeServicesData = [
+        {
+            num: "01",
+            title: "REAL ESTATE",
+            description: "Choose more than a building, choose a home that reflects your personal standard.Live in a space that values distinction, where every detail is held to a high measure.Prioritise lasting worth, quiet elegance, and a seamless fit with the way you live.",
+            img: serviceImg1
+        },
+        {
+            num: "02",
+            title: "INVESTMENT",
+            description: "Criterion Homes offers private, high-yield real estate investment opportunities reserved for a select clientele. Our projects are built on verified legal compliance, future-proof designs, and strategic locations that deliver both financial returns and long-term value preservation.",
+            img: serviceImg2
+        },
+        {
+            num: "03",
+            title: "INFRASTRUCTURE",
+            description: "Choose more than a building, choose a home that reflects your personal standard.Live in a space that values distinction, where every detail is held to a high measure.Prioritise lasting worth, quiet elegance, and a seamless fit with the way you live.",
+            img: serviceImg3
+        },
+        {
+            num: "04",
+            title: "ADVISORY",
+            description: "Choose more than a building, choose a home that reflects your personal standard.Live in a space that values distinction, where every detail is held to a high measure.Prioritise lasting worth, quiet elegance, and a seamless fit with the way you live.",
+            img: serviceImg4
+        },
+    ]
   return (
     <div className={classes.homeServices}>
         <div className={classes.homeServicesDetails}>
             <div className={classes.homeServicesLeftSection}>
                 <h2 data-aos="fade-up"  className={classes.homeServicesHeading}>SERVICES</h2>
                 <div data-aos="fade-up" className={classes.homeServicesImage}>
-                    <img src={homeServiceImg} alt="a block of houses" />
+                    <img src={serviceImg1} alt="a block of houses" />
                 </div>
             </div>
             <div data-aos="fade-up" className={classes.homeServicesRightSection}>
@@ -46,7 +75,7 @@ const HomeServices = () => {
                     </svg>
                 </Button>
                 <div className={classes.homeServicesImage}>
-                    <img src={homeServiceImg} alt="a block of houses" />
+                    <img src={serviceImg1} alt="a block of houses" />
                 </div>
             </div>
     </div>
