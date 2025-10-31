92d50cf7cf634450042308170183d1bc39711f64
Leshi Taiwo <98643006+demi05@users.noreply.github.com>
2025-10-31 06:58:57 +0100

    added proper build folder

--- src/index.tsx
+++ src/index.tsx
@@ -7,36 +7,25 @@ import { BrowserRouter as Router } from "react-router-dom";
 import AppContextProvider from "./Context/AppContext";
 import Toast from "./Components/Toast/Toast";
 
-const isSnap = navigator.userAgent === "ReactSnap";
-
 const rootElement = document.getElementById("root") as HTMLElement;
 
-if (rootElement.hasChildNodes() && !isSnap) {
-  ReactDOM.hydrateRoot(
-    rootElement,
-    <React.StrictMode>
-      <Router>
-        <AppContextProvider>
-          <Toast>
-            <App />
-          </Toast>
-        </AppContextProvider>
-      </Router>
-    </React.StrictMode>
-  );
+const AppWrapper = (
+  <React.StrictMode>
+    <Router>
+      <AppContextProvider>
+        <Toast>
+          <App />
+        </Toast>
+      </AppContextProvider>
+    </Router>
+  </React.StrictMode>
+);
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrateRoot(rootElement, AppWrapper);
 } else {
   const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <Router>
-        <AppContextProvider>
-          <Toast>
-            <App />
-          </Toast>
-        </AppContextProvider>
-      </Router>
-    </React.StrictMode>
-  );
+  root.render(AppWrapper);
 }
 
 reportWebVitals();
